ac61dea84350ef355a246a1e3014b235b2a54cba
Jesse Karnavaara <jesse.karnavaara@edu.lapinamk.fi>
2021-03-11 02:12:15 +0200

    Fixed cases of misspelling and added a closest word if misspelled

--- app.py
+++ app.py
@@ -9,10 +9,28 @@ def value(key):
     #     return print(data[key])
     # else:
     #     return print("There is no such word")
-    if key in data:
-        return print(data[key])
+
+    keyLower = key.lower()
+    i = 0
+    while i < len(keyLower):
+        i += 1
+        if keyLower[:i] in data:
+            if i < len(keyLower):
+                print(
+                    f"The closest word we found for your search was '{keyLower[:i]}'.")
+                return print(data[keyLower[:i]])
+            else:
+                print(f"What does '{keyLower[:i]}' mean?")
+                return print(data[keyLower[:i]])
+        print(i)
+        print(keyLower[:i])
     else:
         return print("There is no such word.")
+
+        # if keyLower[:i] in data:
+        #     return print(data[keyLower])
+        # else:
+        #     return print("There is no such word.")
 
 
 word = input("Enter a word: ")
