50bc0778947a2d56c80e7cec42d6b3cc8ff4788a
Jesse Karnavaara <jesse.karnavaara@edu.lapinamk.fi>
2021-03-11 04:56:38 +0200

    Improved cases of misspelling by adding a similarity ratio using difflib

--- app.py
+++ app.py
@@ -1,5 +1,9 @@
+# import os
 import json
-import os
+import difflib
+
+from difflib import SequenceMatcher
+from difflib import get_close_matches
 
 data = json.load(open("./Interactive English Dictionary/dictionaryData.json"))
 
@@ -9,33 +13,38 @@ def value(key):
     #     return print(data[key])
     # else:
     #     return print("There is no such word")
-
     keyLower = key.lower()
-    i = 0
-    while i < len(keyLower):
-        i += 1
-        if keyLower[:i] in data:
-            if i < len(keyLower):
-                print(
-                    f"The closest word we found for your search was '{keyLower[:i]}'.")
-                return print(data[keyLower[:i]])
-            else:
-                print(f"What does '{keyLower[:i]}' mean?")
-                return print(data[keyLower[:i]])
-        print(i)
-        print(keyLower[:i])
+
+    closeMatches = get_close_matches(keyLower, data, n=1)
+
+    if (keyLower in data):
+        if (len(data[keyLower]) == 1):
+            print(f"The word '{keyLower}' has the following meaning:")
+            return print(data[keyLower])
+        else:
+            print(
+                f"The word '{keyLower}' has '{len(data[keyLower])}' meanings, which are as follows:")
+            return print(data[keyLower])
+    elif (len(closeMatches)):
+        if (len(data[closeMatches[0]]) == 1):
+            print(
+                f"The closest match to '{keyLower}', which we found is '{closeMatches[0]}' and that has the following meaning: \n")
+            return print(data[closeMatches[0]])
+        else:
+            print(
+                f"The closest match to '{keyLower}', which we found is '{closeMatches[0]}' and that has '{len(data[closeMatches[0]])}' meanings: \n")
+            return print(data[closeMatches[0]])
     else:
         return print("There is no such word.")
-
-        # if keyLower[:i] in data:
-        #     return print(data[keyLower])
-        # else:
-        #     return print("There is no such word.")
 
 
 word = input("Enter a word: ")
 
+
 value(word)
+
+
+# print(get_close_matches("rainn", data))
 
 # cwd = os.getcwd()  # Get the current working directory (cwd)
 # files = os.listdir(cwd)  # Get all the files in that directory
