e8e15fa2bbbc337e204bd4197846c7f266d3a0be
Jesse Karnavaara <jesse.karnavaara@edu.lapinamk.fi>
2021-03-11 05:27:50 +0200

    Improved the user interaction by adding proper indentation and numbers

--- app.py
+++ app.py
@@ -9,10 +9,6 @@ data = json.load(open("./Interactive English Dictionary/dictionaryData.json"))
 
 
 def value(key):
-    # if data.get(key):
-    #     return print(data[key])
-    # else:
-    #     return print("There is no such word")
     keyLower = key.lower()
 
     closeMatches = get_close_matches(keyLower, data, n=1)
@@ -20,20 +16,28 @@ def value(key):
     if (keyLower in data):
         if (len(data[keyLower]) == 1):
             print(f"The word '{keyLower}' has the following meaning:")
-            return print(data[keyLower])
+            return print("1. " + data[keyLower][0])
         else:
             print(
-                f"The word '{keyLower}' has '{len(data[keyLower])}' meanings, which are as follows:")
-            return print(data[keyLower])
+                f"The word '{keyLower}' has '{len(data[keyLower])}' meanings, which are as follows: \n")
+            i = 0
+            while (i < len(data[keyLower])):
+                print(f"{i+1}. {data[keyLower][i]}")
+                i += 1
+            return
     elif (len(closeMatches)):
         if (len(data[closeMatches[0]]) == 1):
             print(
                 f"The closest match to '{keyLower}', which we found is '{closeMatches[0]}' and that has the following meaning: \n")
-            return print(data[closeMatches[0]])
+            return print("1. " + data[closeMatches[0]][0])
         else:
             print(
                 f"The closest match to '{keyLower}', which we found is '{closeMatches[0]}' and that has '{len(data[closeMatches[0]])}' meanings: \n")
-            return print(data[closeMatches[0]])
+            i = 0
+            while (i < len(data[closeMatches[0]])):
+                print(f"{i+1}. {data[closeMatches[0]][i]}")
+                i += 1
+            return
     else:
         return print("There is no such word.")
 
